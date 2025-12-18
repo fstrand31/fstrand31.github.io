4742250fdab1d84d58754ad0a9cf1e1fe2d40553
fstrand31 <felicia.johana.strand@gmail.com>
2025-12-18 09:05:12 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -232,7 +232,21 @@ backToTop.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const backToTop = document.getElementById('backToTop');
 
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      backToTop.classList.add('show');
+    } else {
+      backToTop.classList.remove('show');
+    }
+  });
+
+  backToTop.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+});
 
 
 
